49f940f1524f5bad4c5237dc4fb5700557eba800
LittleElijah <cuiyeqing960904@gmail.com>
2018-06-12 23:26:10 +0300

    authentication

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -6,7 +6,9 @@ use App\User;
 use Tymon\JWTAuth\JWTAuth;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Tymon\JWTAuth\Exceptions\{JWTException};
 use App\Http\Requests\Auth\{RegisterFormRequest, LoginFormRequest};
+
 
 class AuthController extends Controller
 {
@@ -15,6 +17,32 @@ class AuthController extends Controller
     public function __construct(JWTAuth $auth)
     {
       $this->auth = $auth;
+    }
+
+    public function login(LoginFormRequest $request)
+    {
+      try {
+        if (!$token = $this->auth->attempt($request->only('email', 'password'))) {
+          return response()->json([
+                    'errors' => [
+                        'root' => 'Could not sign you in with those details.'
+                    ]
+                ], 401);
+        }
+      } catch (JWTException $e) {
+        return response()->json([
+          'errors' => [
+              'root' => 'Failed.'
+          ]
+        ], $e->getStatusCode());
+      }
+
+      return response()->json([
+            'data' => $request->user(),
+            'meta' => [
+                'token' => $token
+            ]
+      ], 200);
     }
 
     public function register(RegisterFormRequest $request)
