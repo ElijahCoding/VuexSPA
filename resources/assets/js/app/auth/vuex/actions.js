7173ce96cbb44cd8794a00d3be0839ec281d4c69
LittleElijah <cuiyeqing960904@gmail.com>
2018-06-14 17:06:15 +0300

    store the token to localstorage

--- resources/assets/js/app/auth/vuex/actions.js
+++ resources/assets/js/app/auth/vuex/actions.js
@@ -8,7 +8,7 @@ export const register = ({ dispatch }, { payload, context }) => {
 
 export const login = ({ dispatch }, { payload, context }) => {
   return axios.post('/api/login', payload).then((response) => {
-    dispatch('setToken').then(() => {
+    dispatch('setToken', response.data.meta.token).then(() => {
       console.log('fetch user');
     })
   }).catch((error) => {
